0fc95984127e23fdaaed7af4b2477196af5c73f4
Kushal Pisavadia <kushi.p@gmail.com>
2013-02-28 19:04:48 +0000

    Add common run-time libs that we'll use

--- project.clj
+++ project.clj
@@ -3,4 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [org.clojure/data.json "0.1.2"]
+                 [compojure "1.1.5"]
+                 [com.novemberain/monger "1.2.0"]
+                 [ring/ring-jetty-adapter "1.1.5"]
+                 [ring/ring-json "0.1.2"]])
