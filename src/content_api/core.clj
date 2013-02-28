a628eb2b3a61e4a02973007ca4993e1357003e2b
Kushal Pisavadia <kushi.p@gmail.com>
2013-02-28 19:16:22 +0000

    Provide a base GET request (homepage)
    Hook up our server so that it responds to a simple GET request on the
    root path.

--- src/content_api/core.clj
+++ src/content_api/core.clj
@@ -1,7 +1,18 @@
 (ns content-api.core
+  (:use [compojure.core]
+        [ring.adapter.jetty]
+        [ring.middleware.json])
+  (:require [compojure.handler :as handler])
   (:gen-class))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defroutes main-routes
+  (GET "/" [] "hello world"))
+
+(def app
+  (handler/site (-> main-routes
+                    wrap-json-params
+                    wrap-json-response)))
+
+(defn -main [& args]
+  (let [port (read-string (get (System/getenv) "GOVUK_APP_PORT" "8080"))]
+    (run-jetty app {:port port})))
