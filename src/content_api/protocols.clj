e46be8a7906f56a0398c6ca39f0f792bc4abd56d
Kushal Pisavadia <kushi.p@gmail.com>
2013-03-26 17:05:27 +0000

    Provide some documentation for protocol method
    This is our "interface", so make the interface easier to understand for
    those that explore using the REPL.

--- src/content_api/protocols.clj
+++ src/content_api/protocols.clj
@@ -1,4 +1,5 @@
 (ns content-api.protocols)
 
 (defprotocol ApiResponse
-  (formatted-response [this]))
+  (formatted-response [this]
+    "Returns the JSON-like structure that we expect to return in the API response"))
